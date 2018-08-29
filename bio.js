cc81ae09a2917c2479c186ea1f30f265e1c5d21f
adellaaye <adellasuryaananda01@gmail.com>
2018-08-29 11:04:10 +0700

    haha

---
+++ bio.js
@@ -0,0 +1,9 @@
+let biodata = {
+    nama: "adella surya ananda",
+    ages: 16,
+    class: "XI",
+    school: "SMK Bina Mandiri Multimedia",
+    hobbies: ["singing, dancing, reading"]
+}
+    console.log( "Nama Saya " + biodata.nama + "\nUmur Saya " + biodata.ages + "\nsaya sekolah di " + biodata.school + "\ndan saya kelas " + biodata.class + "\nhobi saya adalah " + biodata.hobbies )
+
